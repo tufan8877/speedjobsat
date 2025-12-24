7be2c49639843e94bcf894e52e182866d106f037
tufan8877 <tufandonmezyurek@gmail.com>
2025-12-24 17:36:23 +0100

    Create skeleton.tsx

---
+++ components/ui/skeleton.tsx
@@ -0,0 +1,14 @@
+// components/ui/skeleton.tsx
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Skeleton({ className, ...props }: SkeletonProps) {
+  return (
+    <div
+      className={cn("animate-pulse rounded-md bg-muted", className)}
+      {...props}
+    />
+  );
+}
