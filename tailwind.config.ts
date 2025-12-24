42fe59e89ddc3a649558685e5fdbe6bea2c9704b
tufan8877 <tufandonmezyurek@gmail.com>
2025-12-24 18:54:49 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,20 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: [
-    "./index.html",
-    "./main.tsx",
-    "./App.tsx",
-
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./hooks/**/*.{ts,tsx}",
-    "./lib/**/*.{ts,tsx}",
-    "./shared/**/*.{ts,tsx}",
-
-    // falls irgendwo noch "src" existiert:
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ["./**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
