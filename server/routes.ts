6482b5c6252a5acc9fb6b5477689062a8e32ef6b
tufan8877 <tufandonmezyurek@gmail.com>
2026-06-28 17:57:44 +0200

    Use Postgres auth routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,16 +1,13 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { setupSqliteAuth } from "./sqlite-auth";
+import { setupAuth } from "./auth";
 import { setupJobRoutes } from "./job-routes";
 import { setupFavoritesRoutes } from "./favorites-routes";
 import { setupProfileRoutes } from "./profile-routes";
 import { setupAdmin } from "./admin";
 
 export async function registerRoutes(app: Express): Promise<Server> {
-  // SQLite Auth-System einrichten
-  setupSqliteAuth(app);
-  
-  // Alle API-Routen einrichten
+  setupAuth(app);
   setupJobRoutes(app);
   setupFavoritesRoutes(app);
   setupProfileRoutes(app);
