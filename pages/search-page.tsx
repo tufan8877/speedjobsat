cf477de7d3292708e00232dee832af30fae11c50
tufan8877 <tufandonmezyurek@gmail.com>
2026-07-13 11:49:15 +0200

    Use gender-neutral search page title

--- pages/search-page.tsx
+++ pages/search-page.tsx
@@ -17,7 +17,7 @@ export default function SearchPage() {
             <h1 className="text-2xl font-bold mb-6">
               {hasSearchParams 
                 ? "Suchergebnisse" 
-                : "Dienstleister in Österreich finden"}
+                : "Dienstleistungen in Österreich finden"}
             </h1>
             
             <SearchForm />
