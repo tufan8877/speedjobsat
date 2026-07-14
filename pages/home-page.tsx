f71acab3441a3a878ec2eefc41ec48ec6273915c
tufan8877 <tufandonmezyurek@gmail.com>
2026-07-15 00:21:17 +0200

    Restore stable homepage component imports

--- pages/home-page.tsx
+++ pages/home-page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import HeroSection from "@/components/home/hero-section";
-import FeaturedServicesV2 from "@/components/home/featured-services-v2";
+import FeaturedServices from "@/components/home/featured-services";
 import TopProfiles from "@/components/home/top-profiles";
 import ProviderListing from "@/components/home/provider-listing";
 import HowItWorks from "@/components/home/how-it-works";
@@ -12,7 +12,7 @@ export default function HomePage() {
       <Header />
       <main className="flex-1 pb-14 sm:pb-20">
         <HeroSection />
-        <FeaturedServicesV2 />
+        <FeaturedServices />
         <TopProfiles />
         <ProviderListing />
         <HowItWorks />
